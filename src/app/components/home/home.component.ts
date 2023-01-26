c1112102b0663502ce9dee01faf9ce82e7687d2c
Cristian <krhyz02@gmail.com>
2023-01-25 21:29:50 -0300

    Boton volver arriba

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  showButton = false;
+ 
+  timeoutId: any;
+ 
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  @HostListener("document:scroll", [])
+  onScroll() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+    this.timeoutId = setTimeout(() => {
+      const scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      if (scroll >= 100) {
+        this.showButton = true;
+      } else {
+        this.showButton = false;
+      }
+    }, 250);
   }
 
+ 
+ 
 }
+
+
--- src/app/services/educacion.service.ts
+++ src/app/services/educacion.service.ts
@@ -8,8 +8,8 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root',
 })
 export class EducacionService {
-  URL = 'http://localhost:8080/educacion/'
-   /* URL = 'https://portfolio-web-crispens.onrender.com/educacion/'; */
+ /*  URL = 'http://localhost:8080/educacion/' */
+   URL = 'https://portfolio-web-crispens.onrender.com/educacion/';
 
   constructor(private httpClient: HttpClient) {}
 
