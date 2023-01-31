cc91370759d0ec633273112692795e33ab0b3689
Cristian <krhyz02@gmail.com>
2023-01-31 19:21:25 -0300

    styles

--- src/app/services/experiencia.service.ts
+++ src/app/services/experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root',
 })
 export class SExperienciaService {
-/*   expURL = 'http://localhost:8080/explab/' */
+  /* expURL = 'http://localhost:8080/explab/' */
   expURL = 'https://portfolio-web-crispens.onrender.com/explab/';
 
   constructor(private httpClient: HttpClient) {}
