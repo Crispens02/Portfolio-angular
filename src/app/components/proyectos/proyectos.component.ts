5513f54859b0fd67b71453e23cdd54ccff702373
Cristian <krhyz02@gmail.com>
2023-02-04 15:39:40 -0300

    navbar fixes

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Proyecto } from 'src/app/model/proyecto';
 import { ProyectoService } from 'src/app/services/proyecto.service';
 import { TokenService } from 'src/app/services/token.service';
@@ -10,7 +9,7 @@ import { TokenService } from 'src/app/services/token.service';
 })
 export class ProyectosComponent implements OnInit {
   project: Proyecto[] = [];
-  showText = false;
+
   selectedIndex: number;
   isLogged = false;
   constructor(
@@ -46,11 +45,9 @@ export class ProyectosComponent implements OnInit {
   }
   darkenImage(index: number) {
     this.selectedIndex = index;
-    this.showText = true;
   }
 
   restoreImage(index: number) {
     this.selectedIndex = null;
-    this.showText = false;
   }
 }
